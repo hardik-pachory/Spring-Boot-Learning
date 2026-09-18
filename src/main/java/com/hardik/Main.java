da2498fcc87b60e8d4ef236e4f5994d118948a66
hardik-pachory <hardik.19jics082@jietjodhpur.ac.in>
2026-09-18 22:25:19 +0530

    Updating the Application type to none

--- src/main/java/com/hardik/Main.java
+++ src/main/java/com/hardik/Main.java
@@ -4,6 +4,9 @@ package com.hardik;
 //    class act as a Spring Boot Application Entry Point
 // 2. The class should run the SpringBoot Application
 
+// Spring Boot Application has three type of web apps - Servlet, Reactive, and None.
+// None will not start any server.
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
