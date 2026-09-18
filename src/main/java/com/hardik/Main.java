bcdf04f960af404ad88b1431b17e640b950f9d90
hardik-pachory <hardik.19jics082@jietjodhpur.ac.in>
2026-09-18 22:34:18 +0530

    First Endpoint Mapping

--- src/main/java/com/hardik/Main.java
+++ src/main/java/com/hardik/Main.java
@@ -7,13 +7,24 @@ package com.hardik;
 // Spring Boot Application has three type of web apps - Servlet (Default) , Reactive, and None.
 // None will not start any server.
 
+// For any class to have REST EndPoints, the class should have the annotation
+// of the RestController to mark it as the class having endpoint mappings.
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String greet(){
+        return "Welcome to Your SpringBoot App";
     }
 }
