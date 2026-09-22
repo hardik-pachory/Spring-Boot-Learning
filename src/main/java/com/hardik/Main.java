267fe38d662b6a33aa5a4268a5a319a701835585
Hardik Pachory <84628216+hardik-pachory@users.noreply.github.com>
2026-09-23 01:13:51 +0530

    Configuration to Use Postgres (Docker) instead of Fake DB

--- src/main/java/com/hardik/Main.java
+++ src/main/java/com/hardik/Main.java
@@ -13,6 +13,14 @@ package com.hardik;
 // Jackson is the package responsible for converting the Java Objects into the JSON
 // and vice versa.
 
+// After creating the docker-compose file for the project, the following are some useful commands -
+// 1. "docker compose up -d" : creates and starts the service in the docker file in "detached" mode
+//                             that means containers run in the background and your terminal remains usable.
+// 2. "docker compose ps" : ps stands for Process Status. It simply asks docker "What is the current state of the containers belonging to this Compose project?"
+// 3. "docker exec -it postgres bash" : Open an interactive Bash terminal inside my already-running postgres container. 'postgres' is the name of the container that we made
+// 4. "psql -U hardikpac" : This is to switch to your user rather than using the root.
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
